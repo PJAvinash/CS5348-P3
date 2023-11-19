c549dea5cae53d83612e10c01bb628d8a6223e50
PJ Avinash <avinashindian2.0@gmail.com>
2023-11-18 20:14:31 -0600

    commit final

--- csmc.c
+++ csmc.c
@@ -6,6 +6,9 @@
 #include <fcntl.h>
 #include <limits.h>
 
+#define TUTORING_TIME 200
+#define PROGRAMMING_TIME 2000
+
 typedef enum
 {
     PROGRAMMING,
@@ -13,6 +16,18 @@ typedef enum
     BEING_TUTORED
 } StudentState;
 
+/*
+ * Student Structure
+ *
+ * Represents a student in a tutoring system.
+ *
+ * Attributes:
+ *   - id: Unique identifier for the student.
+ *   - tutor_id: Identifier of the tutor currently assisting the student.
+ *   - help: Counter indicating the number of tutoring sessions received.
+ *   - state: Current state of the student (PROGRAMMING, WAITING, BEING_TUTORED).
+ *   - mutex: Mutex for protecting the student's attributes.
+ */
 struct student
 {
     unsigned long id;
@@ -21,6 +36,19 @@ struct student
     StudentState state;
     pthread_mutex_t mutex;
 };
+
+/*
+ * Student Wait Buffer Structure
+ *
+ * Represents a buffer used for managing students in a waiting queue.
+ *
+ * Attributes:
+ *   - size: Maximum size of the buffer.
+ *   - arr: Array of pointers to student structures.
+ *   - is_open: Array indicating whether each position in the buffer is open (1) or occupied (0).
+ *   - open_positions: Number of open positions in the buffer.
+ *   - lock: Mutex for protecting the buffer and its attributes.
+ */
 
 struct student_wait_buffer
 {
@@ -54,19 +82,26 @@ void destroy_wait_buffer(struct student_wait_buffer *buffer)
     {
         return;
     }
-    // Lock the mutex before starting destruction
     pthread_mutex_lock(buffer->lock);
-    // Destroy the mutex
     pthread_mutex_destroy(buffer->lock);
-    // Free the dynamic memory
     free(buffer->arr);
     free(buffer->is_open);
     free(buffer->lock);
-    // Free the buffer itself
     free(buffer);
 }
 
-// returns 1 when successful else 0
+/*
+ * Insert Student into Wait Buffer
+ *
+ * Inserts a student into the waiting buffer if there is an open position.
+ *
+ * Parameters:
+ *   - buffer: Pointer to the student wait buffer.
+ *   - input_student: Pointer to the student to be inserted.
+ *
+ * Returns:
+ *   1 if the insertion is successful, 0 otherwise.
+ */
 int insert(struct student_wait_buffer *buffer, struct student *input_student)
 {
     int is_success = 0;
@@ -92,6 +127,17 @@ int insert(struct student_wait_buffer *buffer, struct student *input_student)
     return is_success;
 }
 
+/*
+ * Pop Student from Wait Buffer
+ *
+ * Pops a student with the highest priority (based on the 'help' attribute) from the waiting buffer.
+ *
+ * Parameters:
+ *   - buffer: Pointer to the student wait buffer.
+ *
+ * Returns:
+ *   A pointer to the student with the highest priority (based on 'help'), or NULL if the buffer is empty.
+ */
 struct student *pop(struct student_wait_buffer *buffer)
 {
     struct student *next_std_ptr = NULL;
@@ -124,32 +170,29 @@ struct student *pop(struct student_wait_buffer *buffer)
     return next_std_ptr;
 }
 
-// struct coordinator_thread_params{
-//     struct student_wait_buffer *buffer;
-//     sem_t* notify_coorinator;
-//     sem_t* notify_student;
-//     pthread_mutex_t *next_student_mutex;
-//     struct student* next_student;
-//     pthread_mutex_t *snapshop_mutex;
-//     int *total_requests;
-//     int *insert_response;
 
-// }
-// void* coordinator_thread(void* args){
-
-// }
-
-struct coordinator_thread_params{
-    struct student_wait_buffer* buffer;
-    struct student** arrived_student;
-    pthread_mutex_t* snapshot_mutex;
-    int* total_requests_ptr;
-    sem_t* chair_occupied;
-    sem_t* student_to_coordinator;
-    sem_t* coordinator_to_student;
-    int* coordinator_thread_return;
+struct coordinator_thread_params
+{
+    struct student_wait_buffer *buffer;
+    struct student **arrived_student;
+    pthread_mutex_t *snapshot_mutex;
+    int *total_requests_ptr;
+    sem_t *chair_occupied;
+    sem_t *student_to_coordinator;
+    sem_t *coordinator_to_student;
+    int *coordinator_thread_return;
 };
-
+/*
+ * Coordinator Thread
+ *
+ * Coordinates the arrival of students and their insertion into the waiting buffer.
+ *
+ * Parameters:
+ *   - args: Pointer to coordinator thread parameters.
+ *
+ * Returns:
+ *   NULL.
+ */
 void *coordinator_thread(void *args)
 {
     while (1)
@@ -170,6 +213,7 @@ void *coordinator_thread(void *args)
         (*params->coordinator_thread_return) = insert_success;
         sem_post(params->coordinator_to_student);
     }
+    return NULL;
 };
 
 /**
@@ -191,12 +235,31 @@ struct student_thread_params
     pthread_mutex_t *snapshop_mutex;
     int *total_requests;
     pthread_mutex_t *coordinator_access;
-    sem_t* student_to_coordinator;
-    sem_t* coordinator_to_student;
-    struct student** coordinator_input;
-    int* coordinator_thread_return;
+    sem_t *student_to_coordinator;
+    sem_t *coordinator_to_student;
+    struct student **coordinator_input;
+    int *coordinator_thread_return;
     int help_max;
 };
+
+/*
+ * Student Thread Function
+ *
+ * This function represents the behavior of a student in the Concurrent Student Management System.
+ * Each student thread executes this function independently. The student creates a struct representing
+ * themselves and enters a loop to request help until the maximum help limit is reached.
+ * 
+ * In each iteration, the student tries to find an available chair by interacting with the coordinator.
+ * If successful, the student takes a seat and enters a waiting state. After being tutored, the student
+ * returns to the programming state and repeats the process until the maximum help limit is reached.
+ * If no chair is available, the student prints a message and tries again later.
+ * 
+ * Parameters:
+ *   - args: A pointer to a struct containing thread-specific parameters (struct student_thread_params).
+ *
+ * Returns:
+ *   - NULL: Indicates the end of the thread's execution.
+ */
 
 void *student_thread(void *args)
 {
@@ -210,7 +273,7 @@ void *student_thread(void *args)
     while (st->help < params->help_max)
     {
         sem_wait(params->chair_available);
-        //int insert_success = insert(params->buffer, st);
+        // int insert_success = insert(params->buffer, st);
         pthread_mutex_lock(params->coordinator_access);
         (*params->coordinator_input) = st;
         int empty_chairs = params->buffer->open_positions;
@@ -219,24 +282,17 @@ void *student_thread(void *args)
         int insert_success = (*params->coordinator_thread_return);
         (*params->coordinator_input) = NULL;
         pthread_mutex_unlock(params->coordinator_access);
-        //int insert_success =  coordinator(params->buffer,st,params->snapshop_mutex,params->total_requests,params->chair_occupied);
         if (insert_success)
         {
-            // pthread_mutex_lock(params->snapshop_mutex);
-            // (*params->total_requests)++;
-            // int total_requests = (*params->total_requests);
-            // int waiting_students = params->buffer->size - params->buffer->open_positions;
-            // pthread_mutex_unlock(params->snapshop_mutex);
-            // sem_post(params->chair_occupied);
-            printf("S: Student %lu takes a seat. Empty chairs = %d\n", st->id, empty_chairs-1);
+            printf("S: Student %lu takes a seat. Empty chairs = %d\n", st->id, empty_chairs - 1);
             st->state = WAITING;
             while (st->state == WAITING)
             {
-                usleep(200);
+                usleep(TUTORING_TIME);
             }
             while (st->state == BEING_TUTORED)
             {
-                usleep(200);
+                usleep(TUTORING_TIME);
             }
             printf("S: Student %lu received help from Tutor %lu.\n", st->id, st->tutor_id);
             st->tutor_id = -1;
@@ -246,26 +302,19 @@ void *student_thread(void *args)
             printf("S: Student %lu found no empty chair,Will try later\n", st->id);
         }
         // go back to programming
-        usleep(2000);
+        usleep(PROGRAMMING_TIME);
     }
     pthread_mutex_lock(params->counter_mutex);
     (*params->st_threads_left)--;
     int threads_left = (*params->st_threads_left);
     pthread_mutex_unlock(params->counter_mutex);
-    if(threads_left == 0){
+    if (threads_left == 0)
+    {
         exit(0);
     }
     return NULL;
 }
 
-/*
- * sem_wait(chair_occupied)
- * pop(fromQ)
- * sem_post(chair_available)
- * increament(help)
- * sem_post(get_tutored)
- * sem_post(tutor_available)
- */
 
 struct tutor_thread_params
 {
@@ -278,6 +327,36 @@ struct tutor_thread_params
     int *total_sessions;
     int *active_sessions;
 };
+
+/*
+ * Tutor Thread Function
+ *.
+ * Parameters:
+ * Thread Parameters Struct (struct tutor_thread_params):
+ *   - buffer: Pointer to the student wait buffer.
+ *   - chair_available: Semaphore indicating available chairs for students.
+ *   - chair_occupied: Semaphore indicating occupied chairs in the tutoring area.
+ *   - counter_mutex: Mutex for protecting the student threads left counter.
+ *   - st_threads_left: Pointer to the counter for remaining student threads.
+ *   - snapshop_mutex: Mutex for protecting shared session statistics.
+ *   - total_sessions: Pointer to the total number of tutoring sessions conducted.
+ *   - active_sessions: Pointer to the number of currently active tutoring sessions.
+ *
+ * Returns:
+ *   - NULL: Indicates the end of the tutor thread's execution.
+ *
+ * Tutoring Process:
+ *   1. Wait for a student to occupy a chair (sem_wait(chair_occupied)).
+ *   2. Retrieve the next student from the tutoring queue (pop(fromQ)).
+ *   3. Release the chair for other students (sem_post(chair_available)).
+ *   4. Increment the student's help counter.
+ *   5. Signal the student to begin the tutoring session (sem_post(get_tutored)).
+ *   6. Signal availability for other tutors (sem_post(tutor_available)).
+ *   7. Repeat the process until all student threads have completed their sessions.
+ *
+ * Note: The function uses usleep(TUTORING_TIME) to simulate the tutoring time.
+ * TUTORING_TIME should be defined appropriately based on the desired tutoring duration.
+ */
 
 void *tutor_thread(void *args)
 {
@@ -305,8 +384,8 @@ void *tutor_thread(void *args)
             next_st->help = next_st->help + 1;
             next_st->tutor_id = thread_id;
             printf("T: Student %lu tutored by %lu. Students tutored now = %d. Total sessions tutored =%d\n", next_st->id, thread_id, active_sessions, total_sessions);
-            usleep(200);                  // teach for 0.2ms;
-            next_st->state = PROGRAMMING; // need a mutex ??
+            usleep(TUTORING_TIME); // teach for 0.2ms;
+            next_st->state = PROGRAMMING; 
             pthread_mutex_lock(params->snapshop_mutex);
             active_sessions = (--*params->active_sessions);
             pthread_mutex_unlock(params->snapshop_mutex);
@@ -314,6 +393,26 @@ void *tutor_thread(void *args)
     }
     return NULL;
 }
+
+/*
+ * Simulation of Concurrent Student Management System
+ *
+ * Creates and manages student and tutor threads along with a coordinator thread
+ * to simulate a concurrent student management system.
+ *
+ * Parameters:
+ *   - students: Number of student threads to create.
+ *   - tutors: Number of tutor threads to create.
+ *   - chairs: Number of available chairs in the waiting area.
+ *   - helplimit: Maximum number of help sessions a student can receive.
+ *
+ * The function initializes semaphores, mutexes, and other necessary variables
+ * for coordination between threads. It then creates student, tutor, and coordinator threads,
+ * simulating the workflow of students seeking help and tutors providing assistance.
+ *
+ * Returns:
+ *   None.
+ */
 void simulatecsms(int students, int tutors, int chairs, int helplimit)
 {
     sem_t chair_available;
@@ -322,8 +421,8 @@ void simulatecsms(int students, int tutors, int chairs, int helplimit)
     sem_t coordinator_to_student;
     sem_init(&chair_occupied, 0, 0);
     sem_init(&chair_available, 0, chairs);
-    sem_init(&student_to_coordinator,0,0);
-    sem_init(&coordinator_to_student,0,0);
+    sem_init(&student_to_coordinator, 0, 0);
+    sem_init(&coordinator_to_student, 0, 0);
 
     int st_threads_left = students;
     pthread_mutex_t st_countex_mutex = PTHREAD_MUTEX_INITIALIZER;
@@ -335,12 +434,12 @@ void simulatecsms(int students, int tutors, int chairs, int helplimit)
     int coordinator_thread_return = 0;
 
     struct student_wait_buffer *swb = initialize_wait_buffer(chairs);
-    struct student* coordinator_input = NULL;
+    struct student *coordinator_input = NULL;
 
-    struct student_thread_params st_params = {swb, &chair_available, &chair_occupied, &st_countex_mutex, &st_threads_left, &snapshop_mutex, &total_requests,&coordinator_access,&student_to_coordinator,&coordinator_to_student,&coordinator_input,&coordinator_thread_return,helplimit};
+    struct student_thread_params st_params = {swb, &chair_available, &chair_occupied, &st_countex_mutex, &st_threads_left, &snapshop_mutex, &total_requests, &coordinator_access, &student_to_coordinator, &coordinator_to_student, &coordinator_input, &coordinator_thread_return, helplimit};
     struct tutor_thread_params tt_params = {swb, &chair_available, &chair_occupied, &st_countex_mutex, &st_threads_left, &snapshop_mutex, &total_sessions, &active_sessions};
-    struct coordinator_thread_params ct_params = {swb,&coordinator_input,&snapshop_mutex,&total_requests,&chair_occupied,&student_to_coordinator,&coordinator_to_student,&coordinator_thread_return};
-    
+    struct coordinator_thread_params ct_params = {swb, &coordinator_input, &snapshop_mutex, &total_requests, &chair_occupied, &student_to_coordinator, &coordinator_to_student, &coordinator_thread_return};
+
     pthread_t student_threads[students];
     pthread_t tutor_threads[tutors];
     pthread_t coordinator_thread_id;
@@ -349,7 +448,7 @@ void simulatecsms(int students, int tutors, int chairs, int helplimit)
     {
         pthread_create(&student_threads[i], NULL, student_thread, (void *)&st_params);
     }
-    pthread_create(&coordinator_thread_id,NULL,coordinator_thread,(void*)&ct_params);
+    pthread_create(&coordinator_thread_id, NULL, coordinator_thread, (void *)&ct_params);
     for (i = 0; i < tutors; i++)
     {
         pthread_create(&tutor_threads[i], NULL, tutor_thread, (void *)&tt_params);
@@ -363,12 +462,24 @@ void simulatecsms(int students, int tutors, int chairs, int helplimit)
     {
         pthread_join(tutor_threads[i], NULL);
     }
-    pthread_join(coordinator_thread_id,NULL);
+    pthread_join(coordinator_thread_id, NULL);
     destroy_wait_buffer(swb);
     sem_destroy(&chair_occupied);
     sem_destroy(&chair_available);
 }
 
+/*
+ *
+ * Command-line Arguments:
+ *   - argv[1]: Number of student threads to create.
+ *   - argv[2]: Number of tutor threads to create.
+ *   - argv[3]: Number of available chairs in the waiting area.
+ *   - argv[4]: Maximum number of help sessions a student can receive.
+ *
+ * Returns:
+ *   - 0: Program executed successfully.
+ *   - -1: Incorrect number of command-line arguments.
+ */
 int main(int argc, char *argv[])
 {
     if (argc != 5)
